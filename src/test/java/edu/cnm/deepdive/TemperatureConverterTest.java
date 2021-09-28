805ce82e3abda8129804cd176f56e235b517f000
zcr003 <zramsey1@cnm.edu>
2021-09-28 10:33:41 -0600

    Implemented multiple test cases by using arrays.

--- src/test/java/edu/cnm/deepdive/TemperatureConverterTest.java
+++ src/test/java/edu/cnm/deepdive/TemperatureConverterTest.java
@@ -6,13 +6,17 @@ import org.junit.jupiter.api.Test;
 
 class TemperatureConverterTest {
 
+  static final double[] celsiusTemperatures = {-40, 0, 100};
+  static final double[] fahrenheitTemperatures = {-40, 32, 212};
+
   @Test
   void convertC2F() {
-    double celsius = -40;
-    double expectedFahrenheit = -40;
-    double actualFahrenheit = TemperatureConverter.convertC2F(celsius);
-    if (expectedFahrenheit != actualFahrenheit) {
-      fail();
+    for (int i = 0; i < celsiusTemperatures.length; i++) {
+      double celsius = celsiusTemperatures[i];
+      double expectedFahrenheit = fahrenheitTemperatures[i];
+      double actualFahrenheit = TemperatureConverter.convertC2F(celsius);
+      assertEquals(expectedFahrenheit, actualFahrenheit);
     }
   }
+
 }
