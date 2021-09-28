d85a9c4807686746ae9ea455d178ebcd4631afe8
zcr003 <zramsey1@cnm.edu>
2021-09-28 14:57:38 -0600

    Implemented F-to-C conversion (opposite conversion) & read from command line (terminal)

--- src/main/java/edu/cnm/deepdive/TemperatureConverter.java
+++ src/main/java/edu/cnm/deepdive/TemperatureConverter.java
@@ -4,18 +4,29 @@ import java.util.Scanner;
 
 public class TemperatureConverter {
 
+  private static final double CELSIUS_FAHRENHEIT_SCALE_FACTOR = 1.8;
+  private static final int CELSIUS_FAHRENHEIT_SCALE_OFFSET = 32;
+
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+    /*Scanner scanner = new Scanner(System.in);
     while (true) {
       System.out.println("Please enter a temperature in Celsius degrees");
       double celsius = scanner.nextDouble();
       double fahrenheit = convertC2F(celsius);
       System.out.println(fahrenheit);
+    }*/
+    for (String arg : args) {
+      double f = Double.parseDouble(arg);
+      System.out.println(convertF2C(f));
     }
   }
 
   public static double convertC2F(double celsius) {
-    return 9 * celsius / 5 + 32;
+    return CELSIUS_FAHRENHEIT_SCALE_FACTOR * celsius + CELSIUS_FAHRENHEIT_SCALE_OFFSET;
+  }
+
+  public static double convertF2C(double fahrenheit) {
+    return (fahrenheit - CELSIUS_FAHRENHEIT_SCALE_OFFSET) / CELSIUS_FAHRENHEIT_SCALE_FACTOR;
   }
 
 }
